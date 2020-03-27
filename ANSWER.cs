e311b24479efd43abd1375ab0837161958c279fe
GoddogProger <62662100+GoddogProger@users.noreply.github.com>
2020-03-27 15:13:44 +0300

    Create ANSWER.cs

---
+++ ANSWER.cs
@@ -0,0 +1,49 @@
+using System;
+
+namespace CAtset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // айди игрока
+            int id = 4;
+
+            // имя игрока
+            string nickName = "BlackBear";
+
+            // первый синвол инени игрока 
+            char firstSymbol = 'B';
+
+            // максимальное значение здоровья установлено на сервером
+            int maxHealth = 100;
+
+            // значение прикотором игрок умирает 
+            int minHealth = 0;
+
+            // Здоровье игрока
+            int playerHealth = 69;
+
+            // броня
+            int armor = 100;
+
+            // патрон В обойме
+            int cartridgesСage = 30;
+
+            // патрон осталось
+            int cartridgeLeft = 69;
+
+            //рост персонажа
+            float growth = 156.4F;
+
+            //Скорость игрока
+            float speed = 72.6F;
+
+            // есть ли оружие у игрока
+            bool Weapons = true;
+
+            // Проверяет игрок жив или мертв
+            bool isDeat = true;
+        }
+    }
+}
